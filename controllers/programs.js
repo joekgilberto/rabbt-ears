b81bcae11fa2570abcf673f450b3bb3935dbd9c0
joegilberto <joekgilberto@gmail.com>
2023-08-13 14:20:49 -0400

    adds media query to poster carousel

--- controllers/programs.js
+++ controllers/programs.js
@@ -6,14 +6,16 @@ module.exports = {
 }
 
 async function index(req, res, next) {
-    let posters = []
-    const fourOhFours = [0, 17, 36, 85, 113, 119, 121, 135, 173, 223, 264, 381, 389, 441, 442]
-    while (posters.length < 50) {
+    let posters = [];
+    const fourOhFours = [0, 17, 36, 85, 113, 119, 121, 135, 173, 223, 264, 381, 389, 441, 442];
+    let used= [];
+    while (posters.length < 51) {
         let rando = Math.floor(Math.random() * 500)
         let current = await fetch(`${Programs.root}/shows/${rando}/images`).then(res => res.json())
 
-        if (!fourOhFours.includes(rando)) {
+        if (!fourOhFours.includes(rando) && !used.includes(rando) && current[0].resolutions.original.width === 680 && current[0].resolutions.original.height === 1000) {
             posters.push(current)
+            used.push(rando)
         }
     }
 
